f17fe13bb34d4b4c7bf019394c729cf8df28bd84
Valentin <carvallegro@users.noreply.github.com>
2019-12-12 13:56:42 +1100

    Created actual test

--- src/App.test.js
+++ src/App.test.js
@@ -3,5 +3,7 @@ import { render } from '@testing-library/react'
 import App from './App'
 
 test('renders learn react link', () => {
-  expect(true).toBeTruthy()
+  const { getByText } = render(<App />)
+  const linkElement = getByText(/rebrew/i)
+  expect(linkElement).toBeInTheDocument()
 })
