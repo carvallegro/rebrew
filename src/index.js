6e0087d2a4f7b87d8669c76d304cec5a3bb49380
Valentin <carvallegro@users.noreply.github.com>
2019-12-12 19:32:22 +1100

    Switched VTEX with Bold UI

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import 'vtex-tachyons'
-
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'bold-ui'
+
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
