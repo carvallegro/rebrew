1006c08cbc5e26a4e2df6e1edfbb778197ab0305
Valentin <carvallegro@users.noreply.github.com>
2020-02-05 15:45:05 +1100

    refactor(spells): Changed Spells Redux slices to add actions

--- src/redux/spells/index.js
+++ src/redux/spells/index.js
@@ -4,7 +4,12 @@ import srdSpells from './srd_spells'
 
 const spellsSlice = createSlice({
   name: 'spells',
-  initialState: srdSpells
+  initialState: srdSpells,
+  reducers: {
+    createSpell: (state, { payload }) => [...state, payload]
+  }
 })
 
 export const spellsReducer = spellsSlice.reducer
+
+export const { createSpell } = spellsSlice.actions
