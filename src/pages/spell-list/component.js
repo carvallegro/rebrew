e2b94ee9c79927b1996d0cd2e48139523a072ce2
Valentin <carvallegro@users.noreply.github.com>
2020-02-07 14:03:28 +1100

    feat(spells): Added exports of all the spells displayed on the page

--- src/pages/spell-list/component.js
+++ src/pages/spell-list/component.js
@@ -1,69 +1,131 @@
 import * as R from 'ramda'
-import React, { Fragment, useEffect, useState } from 'react'
+import copy from 'clipboard-copy'
+
+import React, { Fragment, useMemo, useState } from 'react'
 import PropTypes from 'prop-types'
 import { navigate } from '@reach/router'
 import {
   Button,
+  ButtonGroup,
   Cell,
   Checkbox,
   Grid,
   Heading,
   Icon,
   TextField,
+  Tooltip,
   VFlow
 } from 'bold-ui'
 
 import { spellLevels } from '../../spell-utils'
 import SpellCard from '../../components/spell-card'
 
+const formatResult = R.pipe(
+  R.sortBy(R.prop('name')),
+  R.groupBy(R.prop('level_number'))
+)
+
+const filterByNameCaseInsensitive = searchValue =>
+  R.filter(spell =>
+    R.includes(searchValue.trim().toLowerCase())(spell.name.toLowerCase())
+  )
+
+const SpellList = ({ spellGroups }) => (
+  <Grid gap={2} gapVertical={1} wrap>
+    {Object.keys(spellGroups).length === 0 && (
+      <Cell xs={12}>
+        <Heading level={3}>
+          <i>No result</i>
+        </Heading>
+      </Cell>
+    )}
+    {Object.values(
+      R.mapObjIndexed(
+        (spell, key) => (
+          <Fragment key={key}>
+            <Cell xs={12}>
+              <Heading level={2}>{spellLevels[key].label}</Heading>
+            </Cell>
+            {spell.map(displaySpell)}
+          </Fragment>
+        ),
+        spellGroups
+      )
+    )}
+  </Grid>
+)
+
 export const SpellListPage = ({ spells = [] }) => {
-  const [spellGroups, setSpellGroups] = useState({})
   const [searchValue, updateSearch] = useState('')
   const [includeSrd, setIncludeSrd] = useState(true)
 
-  useEffect(() => {
+  const spellGroups = useMemo(() => {
     if (searchValue.length && searchValue.length < 3) {
-      return
+      return formatResult(spells)
     }
 
-    const searchResult = R.pipe(
+    return R.pipe(
       R.reject(spell => spell.srd && !includeSrd),
-      R.filter(spell =>
-        R.includes(searchValue.trim().toLowerCase())(spell.name.toLowerCase())
-      ),
-      R.sortBy(R.prop('name')),
-      R.groupBy(R.prop('level_number'))
+      filterByNameCaseInsensitive(searchValue),
+      formatResult
     )(spells)
-
-    setSpellGroups(searchResult)
   }, [searchValue, includeSrd, spells])
 
   return (
     <VFlow>
-      {spellListHeader(searchValue, updateSearch, includeSrd, setIncludeSrd)}
+      <Grid gap={0.5} alignItems="center">
+        <Cell lg={3} md={3} sm={4} xs={12}>
+          <TextField
+            icon="zoomOutline"
+            iconPosition="left"
+            name="search-spell-by-name"
+            placeholder="Type spell name"
+            value={searchValue}
+            onChange={e => updateSearch(e.target.value)}
+          />
+        </Cell>
 
-      <Grid gap={2} gapVertical={1} wrap>
-        {Object.keys(spellGroups).length === 0 && (
-          <Cell xs={12}>
-            <Heading level={3}>
-              <i>No result</i>
-            </Heading>
-          </Cell>
-        )}
-        {Object.values(
-          R.mapObjIndexed(
-            (spell, key) => (
-              <Fragment key={key}>
-                <Cell xs={12}>
-                  <Heading level={2}>{spellLevels[key].label}</Heading>
-                </Cell>
-                {spell.map(displaySpell)}
-              </Fragment>
-            ),
-            spellGroups
-          )
-        )}
+        <Cell lg={2} md={3} sm={4} xs={12}>
+          <Checkbox
+            label="Include SRD content"
+            name="include-srd"
+            checked={includeSrd}
+            onChange={() => setIncludeSrd(!includeSrd)}
+          />
+        </Cell>
+
+        <Cell>
+          <ButtonGroup>
+            <Tooltip text="Copy to clipboard" placement="bottom-end">
+              <Button
+                kind="normal"
+                skin="outline"
+                size="medium"
+                style={{ padding: 'calc(0.75rem - 1px)' }}
+                onClick={() =>
+                  copy(
+                    JSON.stringify(Object.values(spellGroups).reduce(R.concat))
+                  )
+                }
+              >
+                <Icon icon="copyOutline" />
+              </Button>
+            </Tooltip>
+            <Button
+              kind="primary"
+              skin="default"
+              size="medium"
+              style={{ padding: 'calc(0.75rem - 1px) 2rem' }}
+              onClick={() => navigate('spells/create')}
+            >
+              <Icon icon="plus" style={{ marginRight: '0.2rem' }} />
+              Add
+            </Button>
+          </ButtonGroup>
+        </Cell>
       </Grid>
+
+      <SpellList spellGroups={spellGroups} />
     </VFlow>
   )
 }
@@ -86,45 +148,4 @@ const displaySpell = (spell, index) => (
       desc={spell.desc}
     />
   </Cell>
-)
-
-const spellListHeader = (
-  searchValue,
-  updateSearch,
-  includeSrd,
-  setIncludeSrd
-) => (
-  <Grid gap={0.5} alignItems="center">
-    <Cell lg={3} md={3} sm={4} xs={12}>
-      <TextField
-        icon="zoomOutline"
-        iconPosition="left"
-        name="search-spell-by-name"
-        placeholder="Type spell name"
-        value={searchValue}
-        onChange={e => updateSearch(e.target.value)}
-      />
-    </Cell>
-
-    <Cell lg={2} md={3} sm={4} xs={12}>
-      <Checkbox
-        label="Include SRD content"
-        name="include-srd"
-        checked={includeSrd}
-        onChange={() => setIncludeSrd(!includeSrd)}
-      />
-    </Cell>
-
-    <Cell lg={1} md={1} sm={1} xs={12}>
-      <Button
-        kind="primary"
-        skin="default"
-        size="medium"
-        onClick={() => navigate('spells/create')}
-      >
-        <Icon icon="plus" style={{ marginRight: '0.5rem' }} />
-        Add
-      </Button>
-    </Cell>
-  </Grid>
 )
