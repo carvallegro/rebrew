912bbf0ac32dc104d1ae7ba0c767a94c0a14396f
Valentin Carmignac <carvallegro@users.noreply.github.com>
2020-02-08 13:02:04 +1100

    fix(spells): issue when trying to edit a spell that does not exist (#43)
    Closes #41

--- src/pages/edit-spell/index.js
+++ src/pages/edit-spell/index.js
@@ -1,32 +1,47 @@
-import React from 'react'
-import SpellForm from '../../components/spell-form'
-import { Breadcrumbs, Link, VFlow } from 'bold-ui'
+import * as R from 'ramda'
+import React, { Fragment } from 'react'
 import { navigate } from '@reach/router'
+import { Breadcrumbs, Link, Text, VFlow } from 'bold-ui'
+
+import SpellForm from '../../components/spell-form'
 import { SpellIntroduction } from '../../components/spell-form/component'
+
 import connect from './connect'
 
-const EditBreadcrumb = () => (
+const EditBreadcrumb = ({ spellName }) => (
   <Breadcrumbs>
     <Link
       color="inherit"
       textDecoration="none"
-      onClick={() => navigate('../spells')}
+      onClick={() => navigate('../../spells')}
     >
       Spells
     </Link>
     <Link color="inherit" textDecoration="none">
-      Edit
+      Edit {spellName}
     </Link>
   </Breadcrumbs>
 )
 
 const EditSpellPage = ({ spellName, spell }) => (
   <VFlow vSpacing={1.5} style={{ maxWidth: '800px' }}>
-    <EditBreadcrumb />
-
-    <SpellIntroduction />
-
-    {spell && <SpellForm mode="edit" spell={spell} />}
+    <EditBreadcrumb spellName={spellName} />
+    {!R.isEmpty(spell) && (
+      <Fragment>
+        <SpellIntroduction />
+        <SpellForm mode="edit" spell={spell} />
+      </Fragment>
+    )}
+    {R.isEmpty(spell) && (
+      <Fragment>
+        <Text component="p" fontSize={1}>
+          There is no spell with that name.
+        </Text>
+        <Text component="p" fontSize={1}>
+          <Link onClick={() => navigate('../../spells')}>Go back</Link>
+        </Text>
+      </Fragment>
+    )}
   </VFlow>
 )
 
