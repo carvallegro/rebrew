6908a37f9a5ab7d331d2c3b97facf2d9711d5e37
carvallegro <carvallegro@users.noreply.github.com>
2020-02-09 23:44:51 +0000

    release v1.4.0

---
+++ precache-manifest.36ee038c7907532262b4e627bad214d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819d5d5d484ddb191fa36c4b2f021323",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5db438c9ef306ef4923",
+    "url": "/static/js/2.e8ec5136.chunk.js"
+  },
+  {
+    "revision": "5a9fdf4e256583cb34d11862e1ead843",
+    "url": "/static/js/2.e8ec5136.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a010141fd6726cfc5a5",
+    "url": "/static/js/main.7807a7f1.chunk.js"
+  },
+  {
+    "revision": "9a944d1bfe681e04457c",
+    "url": "/static/js/runtime-main.b988e60a.js"
+  },
+  {
+    "revision": "d5fbfa905621f56debe9e774cb0a24a7",
+    "url": "/static/media/logo.d5fbfa90.svg"
+  }
+]);
